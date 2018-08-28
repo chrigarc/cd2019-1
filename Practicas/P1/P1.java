72dfab20229d7f04fa6f9a99cb49265b38773295
Brian <jasse626@ciencias.unam.mx>
2018-08-28 15:19:29 -0500

    P1 se agrego pero ya terminado en numero de cuenta

--- Practicas/P1/P1.java
+++ Practicas/P1/P1.java
@@ -1,72 +1,20 @@
-//javac -cp .:../lib/gs-core.jar 
-//javac -cp .:LISTAJAR P1.java
-//java -cp .:LISTAJAR P1
-//.:../lib/gs-core-1.3.jar:../lib/gs-ui-1.3.jar:../lib/gs-algo-1.3.jar
+
 import org.graphstream.graph.*;
-import org.graphstream.graph.implementations.*;
-import org.graphstream.*;
-import org.graphstream.algorithm.generator.RandomGenerator;
-import org.graphstream.algorithm.generator.Generator;
+import org.graphstream.graph.implementations.SingleGraph;
+    
 
 public class P1{
 
-	public static void main(String[] pps){
-		System.setProperty("gs.ui.renderer", "org.graphstream.ui.j2dviewer.J2DGraphRenderer");
-		/**Graph graph = new SingleGraph("Tutorial 1");	
-		graph.addNode("A" );
-		graph.addNode("B" );
-		graph.addNode("C" );
-		graph.addNode("D" );
-		graph.addNode("E" );
-		graph.addEdge("AB", "A", "B");
-		graph.addEdge("AD", "A", "D");
-		graph.addEdge("AE", "A", "E");
-		graph.addEdge("BC", "B", "C");
-		graph.addEdge("BD", "B", "D");
-		graph.addEdge("BE", "B", "E");
-		graph.addEdge("CA", "C", "A");
-		graph.addEdge("CD", "C", "D");
-		graph.addEdge("CE", "C", "E");
-		graph.addEdge("DE", "D", "E");
-		graph.getNode("A").addAttribute("ui.style", "fill-color: rgb(255,0,0);");
-		graph.getNode("B").addAttribute("ui.style", "fill-color: rgb(0,255,0);");
-		graph.getNode("C").addAttribute("ui.style", "fill-color: rgb(0,0,255);");
-		graph.getNode("D").addAttribute("ui.style", "fill-color: rgb(0,100,255);");
-		graph.getNode("E").addAttribute("ui.style", "fill-color: rgb(255,100,0);");
-		graph.display();*/
-		Graph graph = new SingleGraph("Random");
-	    Generator gen = new RandomGenerator(2);
-	    gen.addSink(graph);
-	    gen.begin();
-	    for(int i=0; i<8; i++)
-	        gen.nextEvents();
-	    gen.end();
-	    graph.display();
+  public static void main(String[] pps){
+    Graph graph = new SingleGraph("Tutorial 1");
+    graph.display();
+  }
 
-	    for(Node n:graph) {
-        System.out.println(n.getDegree());
-        if(n.getDegree()==0){
-        	n.addAttribute("ui.style", "fill-color: rgb(255,0,255);");
-
-        }
-        if(n.getDegree()==1){
-        	n.addAttribute("ui.style", "fill-color: rgb(0,0,255);");
-        }
-        if(n.getDegree()==2){
-        	n.addAttribute("ui.style", "fill-color: rgb(255,0,0);");
-        }
-                if(n.getDegree()==3){
-        	n.addAttribute("ui.style", "fill-color: rgb(0,255,0);");
-        }
-                if(n.getDegree()>3){
-        	n.addAttribute("ui.style", "fill-color: rgb(255,0,0);");
-        }
-
-
-    }
-	}
-	
 }
-//ejercicio 2 es generar grafica aleatoria con almenos (10 nodos)
-//pintar de colores distintos todos los nodos co  al menos 1 vecino.
-//pintar de morad todos los nodos sin vecinos.
+//javac  -cp .:LISTAJAR P1.java
+//java  -cp .:LISTAJAR  P1
+
+// .:../lib/gs-core-1.3.jar:../lib/gs-ui-1.3.jar:../lib/gs-algo-1.3.jar
+
+//Class RandomGenerator
+
